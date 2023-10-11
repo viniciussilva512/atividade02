675cf338e4733e5442518c0859cb48c8f1bba0a0
viniciussilva512 <marvosvinicius128@gmail.com>
2023-10-11 11:31:55 -0300

    adicionei atividade02

---
+++ atividade02.py
@@ -0,0 +1,8 @@
+dia1 = "Segunda-Feira"
+dia2 = "Terça-Feira"
+dia3 = "Quarta-Feira"
+dia4 = "Quinta-Feira"
+dia5 = "Sexta-Feira"
+dia6 = "Sábado"
+dia7 = "Domingo"
+print(f'{dia1} {dia2} {dia3} {dia4} {dia5} {dia6} {dia7}')
